5d738aa87368ccb065133dbdafcbde1c604e39e5
Eric Lehman <elehman16>
2023-05-12 06:34:48 -0400

    travis code

---
+++ src/analyze_diagnosis_predictions.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+aa_female_43 = pd.read_csv('output/preds_aa_female_43.csv')
+white_male = pd.read_csv('output/preds_white_male.csv')
+neutral = pd.read_csv('output/preds_completely_neutral.csv')
+
+
+import pdb; pdb.set_trace()
